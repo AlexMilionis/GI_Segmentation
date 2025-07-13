e6a33da64cc084b2614aee61ee5827cfee445bed
AlexMilionis <milionisalexis@gmail.com>
2025-07-13 22:33:31 +0300

    creates script to find optimal learning rate

---
+++ hyperparameter_tuning.py
@@ -0,0 +1,37 @@
+from lightning.pytorch.tuner import Tuner
+from network_module import Net
+import numpy as np
+from hydra.utils import instantiate
+
+def tune_hyperparameters(cfg, trainer, net, dataset, model):
+    
+
+    tuner = Tuner(trainer)
+
+    # Find the optimal learning rate
+    suggested_lrs = []
+    for i in range(3):
+        lr_finder = tuner.lr_find(model=net, datamodule=dataset)
+        suggested_lrs.append(lr_finder.suggestion())
+        # print(f"Run {i+1} suggested LR: {suggested_lrs[-1]}")
+    optimal_lr = np.exp(np.mean(np.log(suggested_lrs)))
+    print(f"Average suggested learning rate: {optimal_lr}")
+
+    # Update scheduler eta_min to 0.1 * optimal_lr if using cosine scheduler
+    cfg.scheduler.eta_min = 0.1 * float(optimal_lr)
+    print(f"Updated scheduler eta_min to: {cfg.scheduler.eta_min}")
+
+    # Recreate the net with updated scheduler config
+    net = Net(
+        model=model,
+        criterion=instantiate(cfg.criterion),
+        optimizer=cfg.optimizer,
+        lr=optimal_lr,
+        scheduler=cfg.scheduler,
+    )
+
+    # Find optimal batch size
+    # batch_size_finder = tuner.scale_batch_size(model=net, datamodule=dataset, mode="binsearch", ckpt_path=os.path.join(logger.log_dir, "batch_size_finder.ckpt"))
+    # print(f"Optimal batch size: {batch_size_finder}")
+
+    return net, optimal_lr
