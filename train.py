c323f7b8814ca3f23c9df16686eddf45e4c99ab0
AlexMilionis <milionisalexis@gmail.com>
2025-07-13 17:40:31 +0300

    optimal learning rate, maximum batch size

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from datamodule import KvasirSEGDataset
 from network_module import Net
 import warnings
 import os
-from visualize_results import visualization_grid, assemble_grid_elements
+from visualize_results import visualization_grid
 
 L.seed_everything(42, workers=True)
 torch.set_float32_matmul_precision("medium")
@@ -26,7 +26,7 @@ def main(cfg):
         img_size = get_efficientnet_image_size(model.model_name)
     else:
         img_size = cfg.img_size
-
+    
     dataset = KvasirSEGDataset(batch_size=cfg.batch_size, img_size=img_size)
 
     net = Net(
@@ -39,18 +39,31 @@ def main(cfg):
 
     trainer = instantiate(cfg.trainer, logger=logger)
 
-    # if efficientnetb5, b6, or b7, use binsearch to find the largest batch size
-    if cfg.model.object.model_name in ["efficientnet-b5", "efficientnet-b6", "efficientnet-b7"]:
-        tuner = Tuner(trainer)
-        tuner.scale_batch_size(net, dataset, mode="binsearch")
+    # # if efficientnetb5, b6, or b7, use binsearch to find the largest batch size
+    # if cfg.model.object.model_name in ["efficientnet-b5", "efficientnet-b6", "efficientnet-b7"]:
+    #     tuner = Tuner(trainer)
+    #     tuner.scale_batch_size(net, dataset, mode="binsearch")
+
+    tuner = Tuner(trainer)
+
+    # Find the optimal learning rate
+    lr_finder = tuner.lr_find(model=net, datamodule=dataset)
+    suggested_lr = lr_finder.suggestion()
+    print(f"Suggested learning rate: {suggested_lr}")
+    fig = lr_finder.plot(suggest=True)
+    logger.experiment.add_figure("LR Finder", fig, global_step=0)
+
+    # Find optimal batch size
+    batch_size_finder = tuner.scale_batch_size(model=net, datamodule=dataset, mode="binsearch")
+    print(f"Optimal batch size: {batch_size_finder}")
     
-    trainer.fit(net, dataset)
-    trainer.test(net, dataset)
-    prediction_outputs = trainer.predict(net, dataset)
+    # trainer.fit(net, dataset)
+    # trainer.test(net, dataset)
+    # prediction_outputs = trainer.predict(net, dataset)
     
-    # Create and log visualization grid
-    logger.experiment.add_figure("Prediction Grid", fig, global_step=trainer.global_step)
-    fig = visualization_grid(prediction_outputs, samples_to_visualize=5)
+    # # Create and log visualization grid
+    # fig = visualization_grid(prediction_outputs, samples_to_visualize=5)
+    # logger.experiment.add_figure("Prediction Grid", fig, global_step=trainer.global_step)
 
 if __name__ == "__main__":
     main()
--- visualize_results.py
+++ visualize_results.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from typing import List, Union
 import torch
 
 def visualization_input():
