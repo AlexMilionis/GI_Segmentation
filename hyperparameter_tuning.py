1968c208268b6cb282275a5a72eb9c795d77ff6a
AlexMilionis <milionisalexis@gmail.com>
2025-07-26 14:52:51 +0300

    tune_hyperparameters

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -2,39 +2,43 @@ from lightning.pytorch.tuner import Tuner
 from network_module import Net
 import numpy as np
 from hydra.utils import instantiate
+import os
 
-def tune_hyperparameters(cfg, trainer, net, dataset, model):
+def tune_hyperparameters(cfg, trainer, net, dataset, logger):
     
-
     tuner = Tuner(trainer)
 
     # Find the optimal learning rate
-    suggested_lrs = []
-    for i in range(3):
-        lr_finder = tuner.lr_find(model=net, datamodule=dataset)
-        suggested_lrs.append(lr_finder.suggestion())
-        # print(f"Run {i+1} suggested LR: {suggested_lrs[-1]}")
-    optimal_lr = np.exp(np.mean(np.log(suggested_lrs)))
-    print(f"Average suggested learning rate: {optimal_lr}")
+    # suggested_lrs = []
+    # for i in range(3):
+    #     lr_finder = tuner.lr_find(model=net, datamodule=dataset)
+    #     suggested_lrs.append(lr_finder.suggestion())
+    #     # print(f"Run {i+1} suggested LR: {suggested_lrs[-1]}")
+    # optimal_lr = np.exp(np.mean(np.log(suggested_lrs)))
+    # print(f"Average suggested learning rate: {optimal_lr}")
+    lr_finder = tuner.lr_find(model=net, datamodule=dataset)
+    optimal_lr = lr_finder.suggestion()
+    logger.experiment.add_scalar("Calculated Learning Rate", optimal_lr, 0)  # Logs at step 0
 
+    cfg.lr = optimal_lr
     # Update scheduler eta_min to 0.1 * optimal_lr if using cosine scheduler
     cfg.scheduler.eta_min = 0.1 * float(optimal_lr)
-    print(f"Updated scheduler eta_min to: {cfg.scheduler.eta_min}")
+    # print(f"Updated scheduler eta_min to: {cfg.scheduler.eta_min}")
 
-    # Recreate the net with updated scheduler config
-    net = Net(
-        model=model,
-        criterion=instantiate(cfg.criterion),
-        optimizer=cfg.optimizer,
-        lr=optimal_lr,
-        scheduler=cfg.scheduler,
-    )
+    # # Recreate the net with updated scheduler config
+    # net = Net(
+    #     model=model,
+    #     criterion=instantiate(cfg.criterion),
+    #     optimizer=cfg.optimizer,
+    #     lr=optimal_lr,
+    #     scheduler=cfg.scheduler,
+    # )
 
-    fig = lr_finder.plot(suggest=True)
+    # fig = lr_finder.plot(suggest=True)
     
 
     # Find optimal batch size
     # batch_size_finder = tuner.scale_batch_size(model=net, datamodule=dataset, mode="binsearch", ckpt_path=os.path.join(logger.log_dir, "batch_size_finder.ckpt"))
     # print(f"Optimal batch size: {batch_size_finder}")
 
-    return net, fig
+    return cfg
--- train.py
+++ train.py
@@ -12,10 +12,8 @@ from network_module import Net
 import warnings
 import os
 from hyperparameter_tuning import tune_hyperparameters
-from visualize_results import visualization_grid
-import matplotlib.pyplot as plt
-import torchvision
-import PIL.Image
+from visualize_results import save_visualization_grid
+
 
 L.seed_everything(42, workers=True)
 torch.set_float32_matmul_precision("medium")
@@ -23,19 +21,12 @@ os.environ["NO_ALBUMENTATIONS_UPDATE"]="1"
 os.environ["HYDRA_FULL_ERROR"]="1"
 warnings.filterwarnings("ignore")
 
-@hydra.main(config_path="config", config_name="overfit_config", version_base=None)
-def main(cfg):
+@hydra.main(config_path="config", config_name="config_unet", version_base=None)
+def main(cfg, load_existing=False):
     logger = loggers.TensorBoardLogger("logs/", name=str(cfg.run_name))
     model = instantiate(cfg.model.object)
-
-    if cfg.img_size == "derived":
-        img_size = get_efficientnet_image_size(model.model_name)
-    else:
-        img_size = cfg.img_size
     
-    dataset = KvasirSEGDataset(batch_size=cfg.batch_size, img_size=img_size)
-
-
+    dataset = KvasirSEGDataset(batch_size=cfg.batch_size, img_size=cfg.img_size)
 
     net = Net(
         model=model,
@@ -44,73 +35,35 @@ def main(cfg):
         lr=cfg.lr,
         scheduler=cfg.scheduler,
     )
+    if load_existing:
+        print("Loading existing model weights...")
+        checkpoint_path = os.path.join(logger.log_dir, "model_checkpoint.ckpt")
+        net = net.load_from_checkpoint(checkpoint_path)
 
     checkpoint_callback = ModelCheckpoint(
         dirpath=logger.log_dir,
-        filename="lr_finder_checkpoint",
+        filename="model_checkpoint",
         save_top_k=1
     )
 
     trainer = instantiate(cfg.trainer, logger=logger, callbacks=[checkpoint_callback])
-
-    # # if efficientnetb5, b6, or b7, use binsearch to find the largest batch size
-    # if cfg.model.object.model_name in ["efficientnet-b5", "efficientnet-b6", "efficientnet-b7"]:
-    #     tuner = Tuner(trainer)
-    #     tuner.scale_batch_size(net, dataset, mode="binsearch")
-
-    # tuner = Tuner(trainer)
-
-    # # Find the optimal learning rate
-    # suggested_lrs = []
-    # for i in range(3):
-    #     lr_finder = tuner.lr_find(model=net, datamodule=dataset)
-    #     suggested_lrs.append(lr_finder.suggestion())
-    #     print(f"Run {i+1} suggested LR: {suggested_lrs[-1]}")
-    # optimal_lr = np.exp(np.mean(np.log(suggested_lrs)))
-    # print(f"Average suggested learning rate: {optimal_lr}")
-
-    # # lr_finder = tuner.lr_find(model=net, datamodule=dataset, ckpt_path=os.path.join(logger.log_dir, "lr_finder.ckpt"))
-    # # suggested_lr = lr_finder.suggestion()
-    # # print(f"Suggested learning rate: {suggested_lr}")
-    # # fig = lr_finder.plot(suggest=True)
-    # # logger.experiment.add_figure("LR Finder", fig, global_step=0)
-
-    # # Find optimal batch size
-    # # batch_size_finder = tuner.scale_batch_size(model=net, datamodule=dataset, mode="binsearch", ckpt_path=os.path.join(logger.log_dir, "batch_size_finder.ckpt"))
-    # # print(f"Optimal batch size: {batch_size_finder}")
-    
-    # net.lr = optimal_lr
-    
-    # # Update scheduler eta_min to 0.1 * optimal_lr if using cosine scheduler
-    # cfg.scheduler.eta_min = 0.1 * optimal_lr
-    # print(f"Updated scheduler eta_min to: {cfg.scheduler.eta_min}")
-    
-    # # Recreate the net with updated scheduler config
-    # net = Net(
-    #     model=model,
-    #     criterion=instantiate(cfg.criterion),
-    #     optimizer=cfg.optimizer,
-    #     lr=optimal_lr,
-    #     scheduler=cfg.scheduler,
-    # )
-
-    # net, lr_figure = tune_hyperparameters(cfg, trainer, net, dataset, model)
-    # logger.experiment.add_figure("LR Finder", lr_figure, global_step=0)
+    cfg = tune_hyperparameters(cfg, trainer, net, dataset, logger)
+    net.lr = cfg.lr
+    # Recreate the net with updated scheduler config
+    net = Net(
+        model=model,
+        criterion=instantiate(cfg.criterion),
+        optimizer=cfg.optimizer,
+        lr=cfg.lr,
+        scheduler=cfg.scheduler,
+    )
 
     trainer.fit(net, dataset)
     trainer.test(net, dataset)
     prediction_outputs = trainer.predict(net, dataset)
     
     # Create and log visualization grid
-    fig = visualization_grid(prediction_outputs)
-    fig_path = os.path.join(logger.log_dir, "prediction_grid.png")
-    fig.savefig(fig_path)
-    plt.close(fig) 
-    # logger.experiment.add_figure("Prediction Grid", fig, global_step=trainer.global_step)
-
-    # image = PIL.Image.open(fig_path)
-    # image = torchvision.transforms.ToTensor()(image)
-    # logger.experiment.add_image("Prediction Grid", image, global_step=trainer.global_step)
+    save_visualization_grid(prediction_outputs, logger.log_dir)
 
 
 if __name__ == "__main__":
